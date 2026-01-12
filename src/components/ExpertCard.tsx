5f2daf5c44101bd81718e2f8824379b3f427d1ea
Sultan Alkhubayzi <Sultan.Bus@outlook.com>
2026-01-12 22:47:14 +0300

    fix: image

--- src/components/ExpertCard.tsx
+++ src/components/ExpertCard.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import soonImage from "@/assets/soon.jpeg";
+import soonImage from "@/assets/soon.png";
 
 interface ExpertCardProps {
   title: string;
